744fe2657f67fe147709dd650c48268657aa4492
Noba1anc3 <zxryhjp@yahoo.co.jp>
2020-10-14 09:33:55 +0800

    paddleocr

---
+++ PaddleOCR-develop/tools/infer/predict_system.py
@@ -0,0 +1,276 @@
+# Copyright (c) 2020 PaddlePaddle Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+import time
+
+__dir__ = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(__dir__)
+sys.path.append(os.path.abspath(os.path.join(__dir__, '../..')))
+
+import tools.infer.utility as utility
+from ppocr.utils.utility import initial_logger
+
+logger = initial_logger()
+import cv2
+import tools.infer.predict_det as predict_det
+import tools.infer.predict_cls as predict_cls
+import copy
+import numpy as np
+import math
+from ppocr.utils.utility import get_image_file_list, check_and_read_gif
+
+
+class TextSystem(object):
+    def __init__(self, args):
+        self.text_detector = predict_det.TextDetector(args)
+        # self.text_recognizer = predict_rec.TextRecognizer(args)
+        self.use_angle_cls = args.use_angle_cls
+        if self.use_angle_cls:
+            self.text_classifier = predict_cls.TextClassifier(args)
+
+    def cal_ver_hor_edge(self, dt_box):
+        if dt_box[1][0] - dt_box[0][0] == 0:
+            line_up = 90
+        else:
+            line_up = (dt_box[1][1] - dt_box[0][1]) / (dt_box[1][0] - dt_box[0][0])
+            line_up = math.fabs(int(math.atan(line_up) * 180 / math.pi))
+        if dt_box[2][0] - dt_box[1][0] == 0:
+            line_rt = 90
+        else:
+            line_rt = (dt_box[2][1] - dt_box[1][1]) / (dt_box[2][0] - dt_box[1][0])
+            line_rt = math.fabs(int(math.atan(line_rt) * 180 / math.pi))
+        if dt_box[3][0] - dt_box[2][0] == 0:
+            line_dn = 90
+        else:
+            line_dn = (dt_box[3][1] - dt_box[2][1]) / (dt_box[3][0] - dt_box[2][0])
+            line_dn = math.fabs(int(math.atan(line_dn) * 180 / math.pi))
+        if dt_box[0][0] - dt_box[3][0] == 0:
+            line_lt = 90
+        else:
+            line_lt = (dt_box[0][1] - dt_box[3][1]) / (dt_box[0][0] - dt_box[3][0])
+            line_lt = math.fabs(int(math.atan(line_lt) * 180 / math.pi))
+
+        if line_up <= min(line_rt, line_dn, line_lt):
+            horizonal_edge = math.sqrt(pow(dt_box[0][0] - dt_box[1][0], 2) + pow(dt_box[0][1] - dt_box[1][1], 2))
+        elif line_rt <= min(line_up, line_dn, line_lt):
+            horizonal_edge = math.sqrt(pow(dt_box[2][0] - dt_box[1][0], 2) + pow(dt_box[2][1] - dt_box[1][1], 2))
+        elif line_dn <= min(line_rt, line_up, line_lt):
+            horizonal_edge = math.sqrt(pow(dt_box[2][0] - dt_box[3][0], 2) + pow(dt_box[2][1] - dt_box[3][1], 2))
+        elif line_lt <= min(line_rt, line_dn, line_up):
+            horizonal_edge = math.sqrt(pow(dt_box[0][0] - dt_box[3][0], 2) + pow(dt_box[0][1] - dt_box[3][1], 2))
+
+        if line_up >= max(line_rt, line_dn, line_lt):
+            vertical_edge = math.sqrt(pow(dt_box[0][0] - dt_box[1][0], 2) + pow(dt_box[0][1] - dt_box[1][1], 2))
+        elif line_rt >= max(line_up, line_dn, line_lt):
+            vertical_edge = math.sqrt(pow(dt_box[2][0] - dt_box[1][0], 2) + pow(dt_box[2][1] - dt_box[1][1], 2))
+        elif line_dn >= max(line_rt, line_up, line_lt):
+            vertical_edge = math.sqrt(pow(dt_box[2][0] - dt_box[3][0], 2) + pow(dt_box[2][1] - dt_box[3][1], 2))
+        elif line_lt >= max(line_rt, line_dn, line_up):
+            vertical_edge = math.sqrt(pow(dt_box[0][0] - dt_box[3][0], 2) + pow(dt_box[0][1] - dt_box[3][1], 2))
+
+        return vertical_edge, horizonal_edge
+
+    def get_rotate_crop_image(self, img, dt_box):
+        """
+        img_height, img_width = img.shape[0:2]
+        left = int(np.min(points[:, 0]))
+        right = int(np.max(points[:, 0]))
+        top = int(np.min(points[:, 1]))
+        bottom = int(np.max(points[:, 1]))
+        img_crop = img[top:bottom, left:right, :].copy()
+        points[:, 0] = points[:, 0] - left
+        points[:, 1] = points[:, 1] - top
+        """
+
+        vertical_edge, horizonal_edge = self.cal_ver_hor_edge(dt_box)
+        vertical_edge = int(vertical_edge)
+        horizonal_edge = int(horizonal_edge)
+        img_crop_width = int(
+            max(
+                np.linalg.norm(dt_box[0] - dt_box[1]),
+                np.linalg.norm(dt_box[2] - dt_box[3])))
+        img_crop_height = int(
+            max(
+                np.linalg.norm(dt_box[0] - dt_box[3]),
+                np.linalg.norm(dt_box[1] - dt_box[2])))
+
+        status = 0
+        if img_crop_height / img_crop_width < 1 and vertical_edge / horizonal_edge > 1:
+            status = 1
+        elif img_crop_height / img_crop_width > 1 and vertical_edge / horizonal_edge < 1:
+            status = 1
+
+        pts_std = np.float32([[0, 0], [img_crop_width, 0],
+                              [img_crop_width, img_crop_height],
+                              [0, img_crop_height]])
+        M = cv2.getPerspectiveTransform(dt_box, pts_std)
+        dst_img = cv2.warpPerspective(
+            img,
+            M, (img_crop_width, img_crop_height),
+            borderMode=cv2.BORDER_REPLICATE,
+            flags=cv2.INTER_CUBIC)
+
+        dst_img_height, dst_img_width = dst_img.shape[0:2]
+        if dst_img_height * 1.0 / dst_img_width >= 1.5:
+            dst_img = np.rot90(dst_img)
+        
+        wh_ratio = False
+        dst_img_height, dst_img_width = dst_img.shape[0:2]
+        if dst_img_width *1.0 / dst_img_height > 2:
+            wh_ratio = True
+
+        if status:
+            if wh_ratio:
+                return dst_img, True, True
+            else:
+                return dst_img, True, False
+        else:
+            if wh_ratio:
+                return dst_img, False, True
+            else:
+                return dst_img, False, False
+
+    def print_draw_crop_rec_res(self, img_crop_list, rec_res):
+        bbox_num = len(img_crop_list)
+        for bno in range(bbox_num):
+            cv2.imwrite("./output/img_crop_%d.jpg" % bno, img_crop_list[bno])
+            print(bno, rec_res[bno])
+
+    def __call__(self, img):
+        ori_im = img.copy()
+        dt_boxes, elapse = self.text_detector(img)
+        with open('result.txt', 'a+') as f:
+            f.write(' ' + '%.2f' % (elapse*1000))
+        print("dt_boxes num : {}, elapse : {}ms"
+        .format(len(dt_boxes), '%.2f' % (elapse*1000)))
+  
+        mid_time = time.time()
+
+        left_right = up_down = 0
+
+        for dt_box in dt_boxes:
+            vertical_edge, horizonal_edge = self.cal_ver_hor_edge(dt_box)
+
+            if horizonal_edge < vertical_edge:
+                left_right += vertical_edge / horizonal_edge
+            else:
+                up_down += horizonal_edge / vertical_edge
+
+        print('上下 :', up_down, '左右 :', left_right)
+
+        if dt_boxes is None:
+            return None, None
+        dt_boxes = sorted_boxes(dt_boxes)
+        for i, dt_box in enumerate(dt_boxes):
+            vertical_edge, horizonal_edge = self.cal_ver_hor_edge(dt_box)
+            if up_down > left_right and vertical_edge/horizonal_edge > 1:
+                del dt_boxes[i]
+                print('过滤宽高比不一致的检测框')
+            elif up_down <= left_right and vertical_edge/horizonal_edge < 1:
+                del dt_boxes[i]
+                print('过滤宽高比不一致的检测框')
+
+        count = 0
+        img_crop_list = []
+        for bno in range(len(dt_boxes)):
+            tmp_box = copy.deepcopy(dt_boxes[bno])
+            img_crop, status, wh_ratio = self.get_rotate_crop_image(ori_im, tmp_box)
+            if wh_ratio:
+                img_crop_list.append(img_crop)
+                if status:
+                    count += 1
+
+        with open('result.txt', 'a+') as f:
+            f.write(' ' + '%.2f' % ((time.time() - mid_time) * 1000))
+        print("cls num : {}, elapse : {}ms".format(
+            len(img_crop_list), '%.2f' % ((time.time() - mid_time)*1000)))
+
+        if len(dt_boxes) == 0:
+            with open('result.txt', 'a+') as f:
+                f.write(' 0')
+        else:
+            print('错误仿射变换占比:', '%.2f' % (count*100 / len(dt_boxes)))
+            with open('result.txt', 'a+') as f:
+                f.write(' ' + '%.2f' % (count*100 / len(dt_boxes)))
+
+        if self.use_angle_cls:
+            img_crop_list, angle_list, elapse, lr = self.text_classifier(
+                img_crop_list)
+
+            with open('result.txt', 'a+') as f:
+                f.write(' ' + '%.2f' % (elapse * 1000))
+            print("cls num : {}, elapse : {}ms".format(
+                len(img_crop_list), '%.2f' % (elapse*1000)))
+
+            with open('result.txt', 'a+') as f:
+                if left_right >= up_down and lr == 1:
+                    print('右\n')
+                    f.write(' ' + str(3) + '\n')
+                elif left_right >= up_down and lr == 0:
+                    print('左\n')
+                    f.write(' ' + str(1) + '\n')
+                elif left_right < up_down and lr == 1:
+                    print('上\n')
+                    f.write(' ' + str(0) + '\n')
+                elif left_right < up_down and lr == 0:
+                    print('下\n')
+                    f.write(' ' + str(2) + '\n')
+
+        return dt_boxes, 1
+
+
+def sorted_boxes(dt_boxes):
+    """
+    Sort text boxes in order from top to bottom, left to right
+    args:
+        dt_boxes(array):detected text boxes with shape [4, 2]
+    return:
+        sorted boxes(array) with shape [4, 2]
+    """
+    num_boxes = dt_boxes.shape[0]
+    sorted_boxes = sorted(dt_boxes, key=lambda x: (x[0][1], x[0][0]))
+    _boxes = list(sorted_boxes)
+
+    for i in range(num_boxes - 1):
+        if abs(_boxes[i + 1][0][1] - _boxes[i][0][1]) < 10 and \
+                (_boxes[i + 1][0][0] < _boxes[i][0][0]):
+            tmp = _boxes[i]
+            _boxes[i] = _boxes[i + 1]
+            _boxes[i + 1] = tmp
+    return _boxes
+
+
+def main(args):
+    image_file_list = get_image_file_list(args.image_dir)
+    text_sys = TextSystem(args)
+    for image_file in sorted(image_file_list):
+
+        with open('result.txt', 'a+') as f:
+            f.write(image_file.split('noangle/')[1])
+        print(image_file.split('noangle/')[1])
+
+        img, flag = check_and_read_gif(image_file)
+        if not flag:
+            img = cv2.imread(image_file)
+        if img is None:
+            logger.info("error in loading image:{}".format(image_file))
+            continue
+        text_sys(img)
+
+
+if __name__ == "__main__":
+    main(utility.parse_args())
+
---
+++ PaddleOCR-develop/tools/infer/result_analysis_standard.py
@@ -0,0 +1,324 @@
+
+
+num = {'chepiao': [0, 0, 0, 0, 0, 0, 0, 0],
+       'chuzuchefapiao': [0, 0, 0, 0, 0, 0, 0, 0],
+       'dianzishu': [0, 0, 0, 0, 0, 0, 0, 0],
+       'jiaocai': [0, 0, 0, 0, 0, 0, 0, 0],
+       'jipiao': [0, 0, 0, 0, 0, 0, 0, 0],
+       'menpiao': [0, 0, 0, 0, 0, 0, 0, 0],
+       'mingpian': [0, 0, 0, 0, 0, 0, 0, 0],
+       'muji': [0, 0, 0, 0, 0, 0, 0, 0],
+       'niandubaogao': [0, 0, 0, 0, 0, 0, 0, 0],
+       'shenfenzheng': [0, 0, 0, 0, 0, 0, 0, 0],
+       'shijuan': [0, 0, 0, 0, 0, 0, 0, 0],
+       'shouju': [0, 0, 0, 0, 0, 0, 0, 0],
+       'tijianbiao': [0, 0, 0, 0, 0, 0, 0, 0],
+       'jiashizheng': [0, 0, 0, 0, 0, 0, 0, 0],
+       'yingyezhizhao': [0, 0, 0, 0, 0, 0, 0, 0],
+       'yinhangliushui': [0, 0, 0, 0, 0, 0, 0, 0],
+       'zengzhishuifapiao': [0, 0, 0, 0, 0, 0, 0, 0],
+       'pingjibaogao': [0, 0, 0, 0, 0, 0, 0, 0],
+       }
+
+Piaoju = {'chepiao', 'chuzuchefapiao', 'jipiao', 'menpiao', 'shouju', 'zengzhishuifapiao'}
+piaoju = [0, 0, 0, 0, 0, 0, 0, 0]
+Certificate = {'mingpian', 'shenfenzheng', 'jiashizheng', 'yingyezhizhao'}
+certificate = [0, 0, 0, 0, 0, 0, 0, 0]
+Docu = {'dianzishu', 'jiaocai', 'muji', 'yinhangliushui', 'pingjibaogao', 'niandubaogao'}
+docu = [0, 0, 0, 0, 0, 0, 0, 0]
+Other = {'shijuan', 'tijianbiao'}
+other = [0, 0, 0, 0, 0, 0, 0, 0]
+
+labels = []
+preds = []
+
+with open('./label.txt', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+           labels.append([line.split(' ')[0], line.split(' ')[1].split('\n')[0]])
+f.close()
+
+with open('./result.txt', 'r') as f:
+    lines = f.readlines()
+    for line in lines:
+           preds.append([line.split(' ')[0], line.split(' ')[3].split('\n')[0], line.split(' ')[1], line.split(' ')[2]])
+f.close()
+
+phase_1_time = 0
+phase_1_min = 1000
+phase_1_max = 0
+phase_2_time = 0
+phase_2_min = 1000
+phase_2_max = 0
+total_time = 0
+total_min = 1000
+total_max = 0
+
+minus300 = 0
+minus500 = 0
+minus1000 = 0
+minus2000 = 0
+
+for i, pred in enumerate(preds):
+       phase_1_time += float(pred[2])
+       phase_2_time += float(pred[3])
+       total_time += float(pred[2]) + float(pred[3])
+
+       if float(pred[2]) + float(pred[3]) < 300:
+           minus300 += 1
+       elif float(pred[2]) + float(pred[3]) < 500:
+           minus500 += 1
+       elif float(pred[2]) + float(pred[3]) < 1000:
+           minus1000 += 1
+       elif float(pred[2]) + float(pred[3]) < 2000:
+           minus2000 += 1
+
+       if float(pred[2]) < phase_1_min:
+           phase_1_min = float(pred[2])
+       if float(pred[2]) > phase_1_max:
+           phase_1_max = float(pred[2])
+       if float(pred[3]) < phase_2_min:
+           phase_2_min = float(pred[3])
+       if float(pred[3]) > phase_2_max:
+           phase_2_max = float(pred[3])
+       if float(pred[2]) + float(pred[3]) < total_min:
+           total_min = float(pred[2]) + float(pred[3])
+       if float(pred[2]) + float(pred[3]) > total_max:
+           total_max = float(pred[2]) + float(pred[3])
+
+       pred_name = pred[0].split('.jp')[0]
+       label = ''
+       for l in labels:
+              if l[0] == pred_name:
+                     label = l
+                     break
+       #label = labels[i]
+       cls = pred[0].split('_')[0]
+       if cls == 'yibao':
+              cls = 'jiashizheng'
+
+       label_direction = int(label[1])
+       pred_direction = int(pred[1])
+       num[cls][label_direction*2+1] += 1
+       if label_direction == pred_direction:
+              num[cls][pred_direction*2] += 1
+
+print('\n检测时间最少:', phase_1_min, '检测时间最多:', phase_1_max, '检测时间平均:', '%.2f' % (phase_1_time / 3600))
+print('分类时间最少:', phase_2_min, '分类时间最多:', phase_2_max, '分类时间平均:', '%.2f' % (phase_2_time / 3600))
+print('总时间最少:', total_min, '总时间最多:', total_max, '总时间平均:', '%.2f' % (total_time / 3600))
+print('低于300ms:', '%.2f' % (minus300/36) + '%', '低于500ms:', '%.2f' % (minus500/36) + '%', '低于1000ms:', '%.2f' % (minus1000/36) + '%', '低于2000ms', '%.2f' % (minus2000/36) + '%')
+all_correct = 0
+all = 0
+for key in num:
+       all_correct += (num[key][0]+num[key][2]+num[key][4]+num[key][6])
+       all += (num[key][1]+num[key][3]+num[key][5]+num[key][7])
+
+print('\n总准确率:', '%.2f' % (all_correct*100 / all), '\n')
+
+for i, key in enumerate(Piaoju):
+       piaoju[0] += num[key][0]
+       piaoju[1] += num[key][1]
+       piaoju[2] += num[key][2]
+       piaoju[3] += num[key][3]
+       piaoju[4] += num[key][4]
+       piaoju[5] += num[key][5]
+       piaoju[6] += num[key][6]
+       piaoju[7] += num[key][7]
+
+print('票据\n',
+      '%.2f' % (piaoju[0]*100/piaoju[1]),
+      '%.2f' % (piaoju[4]*100/piaoju[5]),
+      '%.2f' % (piaoju[2]*100/piaoju[3]),
+      '%.2f' % (piaoju[6]*100/piaoju[7]),
+      '%.2f' % ((piaoju[0] + piaoju[2] + piaoju[4] + piaoju[6]) / 12), '\n')
+
+for i, key in enumerate(Certificate):
+       certificate[0] += num[key][0]
+       certificate[1] += num[key][1]
+       certificate[2] += num[key][2]
+       certificate[3] += num[key][3]
+       certificate[4] += num[key][4]
+       certificate[5] += num[key][5]
+       certificate[6] += num[key][6]
+       certificate[7] += num[key][7]
+
+print('证件\n',
+      '%.2f' % (certificate[0]*100/certificate[1]),
+      '%.2f' % (certificate[4]*100/certificate[5]),
+      '%.2f' % (certificate[2]*100/certificate[3]),
+      '%.2f' % (certificate[6]*100/certificate[7]),
+      '%.2f' % ((certificate[0] + certificate[2] + certificate[4] + certificate[6]) / 8), '\n')
+
+for i, key in enumerate(Docu):
+       docu[0] += num[key][0]
+       docu[1] += num[key][1]
+       docu[2] += num[key][2]
+       docu[3] += num[key][3]
+       docu[4] += num[key][4]
+       docu[5] += num[key][5]
+       docu[6] += num[key][6]
+       docu[7] += num[key][7]
+
+print('文档\n',
+      '%.2f' % (docu[0]*100/docu[1]),
+      '%.2f' % (docu[4]*100/docu[5]),
+      '%.2f' % (docu[2]*100/docu[3]),
+      '%.2f' % (docu[6]*100/docu[7]),
+      '%.2f' % ((docu[0] + docu[2] + docu[4] + docu[6]) / 12), '\n')
+
+for i, key in enumerate(Other):
+       other[0] += num[key][0]
+       other[1] += num[key][1]
+       other[2] += num[key][2]
+       other[3] += num[key][3]
+       other[4] += num[key][4]
+       other[5] += num[key][5]
+       other[6] += num[key][6]
+       other[7] += num[key][7]
+
+print('其他\n',
+      '%.2f' % (other[0]*100/other[1]),
+      '%.2f' % (other[4]*100/other[5]),
+      '%.2f' % (other[2]*100/other[3]),
+      '%.2f' % (other[6]*100/other[7]),
+      '%.2f' % ((other[0] + other[2] + other[4] + other[6]) / 4), '\n')
+
+print('方向 上 ',
+      '%.2f' % ((other[0] + docu[0] + certificate[0] + piaoju[0])*100 / (other[1] + docu[1] + certificate[1] + piaoju[1])),
+      '\n方向 下 ',
+      '%.2f' % ((other[4] + docu[4] + certificate[4] + piaoju[4])*100 / (other[5] + docu[5] + certificate[5] + piaoju[5])),
+      '\n方向 左 ',
+      '%.2f' % ((other[2] + docu[2] + certificate[2] + piaoju[2])*100 / (other[3] + docu[3] + certificate[3] + piaoju[3])),
+      '\n方向 右 ',
+      '%.2f' % ((other[6] + docu[6] + certificate[6] + piaoju[6])*100 / (other[7] + docu[7] + certificate[7] + piaoju[7])),
+      '\n')
+
+for i, key in enumerate(num):
+    if key in Piaoju:
+        print('| ' + key + ' | ',
+              '%.2f' % (num[key][0]*100 / num[key][1]) + ' |',
+              '%.2f' % (num[key][4]*100 / num[key][5]) + ' |',
+              '%.2f' % (num[key][2]*100 / num[key][3]) + ' |',
+              '%.2f' % (num[key][6]*100 / num[key][7]) + ' |',
+              '%.2f' % ((num[key][0] + num[key][2] + num[key][4] + num[key][6])*100 /
+                                (num[key][1] + num[key][3] + num[key][5] + num[key][7])) + ' |')
+print('\n')
+
+for i, key in enumerate(num):
+    if key in Certificate:
+        print('| ' + key + ' | ',
+              '%.2f' % (num[key][0]*100 / num[key][1]) + ' |',
+              '%.2f' % (num[key][4]*100 / num[key][5]) + ' |',
+              '%.2f' % (num[key][2]*100 / num[key][3]) + ' |',
+              '%.2f' % (num[key][6]*100 / num[key][7]) + ' |',
+              '%.2f' % ((num[key][0] + num[key][2] + num[key][4] + num[key][6])*100 /
+                                (num[key][1] + num[key][3] + num[key][5] + num[key][7])) + ' |')
+print('\n')
+
+for i, key in enumerate(num):
+    if key in Docu:
+        print('| ' + key + ' | ',
+              '%.2f' % (num[key][0]*100 / num[key][1]) + ' |',
+              '%.2f' % (num[key][4]*100 / num[key][5]) + ' |',
+              '%.2f' % (num[key][2]*100 / num[key][3]) + ' |',
+              '%.2f' % (num[key][6]*100 / num[key][7]) + ' |',
+              '%.2f' % ((num[key][0] + num[key][2] + num[key][4] + num[key][6])*100 /
+                                (num[key][1] + num[key][3] + num[key][5] + num[key][7])) + ' |')
+print('\n')
+
+for i, key in enumerate(num):
+    if key in Other:
+        print('| ' + key + ' | ',
+              '%.2f' % (num[key][0]*100 / num[key][1]) + ' |',
+              '%.2f' % (num[key][4]*100 / num[key][5]) + ' |',
+              '%.2f' % (num[key][2]*100 / num[key][3]) + ' |',
+              '%.2f' % (num[key][6]*100 / num[key][7]) + ' |',
+              '%.2f' % ((num[key][0] + num[key][2] + num[key][4] + num[key][6])*100 /
+                                (num[key][1] + num[key][3] + num[key][5] + num[key][7])) + ' |')
+
+
+doc_correct, lr_correct, ud_correct, lr_total, ud_total = 0, 0, 0, 0, 0
+for i, pred in enumerate(preds):
+
+    pred_name = pred[0].split('.jp')[0]
+    label = ''
+    for l in labels:
+        if l[0] == pred_name:
+            label = l
+            break
+
+    if label[1] in ['0', '2']:
+        lr_total += 1
+        if pred[1] in ['0', '2']:
+            doc_correct += 1
+            lr_correct += 1
+    elif label[1] in ['1', '3']:
+        ud_total += 1
+        if pred[1] in ['1', '3']:
+            doc_correct += 1
+            ud_correct += 1
+
+print('\n文档朝向准确率:', '%.2f' % (doc_correct/36))
+print('上下朝向准确率:', '%.2f' % (lr_correct*100/lr_total))
+print('左右朝向准确率:', '%.2f' % (ud_correct*100/ud_total))
+
+dir_correct, forward_correct, reverse_correct, forward_total, reverse_total = 0, 0, 0, 0, 0
+for i, pred in enumerate(preds):
+
+    pred_name = pred[0].split('.jp')[0]
+    label = ''
+    for l in labels:
+        if l[0] == pred_name:
+            label = l
+            break
+
+    if label[1] in ['1', '2']:
+        reverse_total += 1
+        if pred[1] in ['1', '2']:
+            dir_correct += 1
+            reverse_correct += 1
+    elif label[1] in ['0', '3']:
+        forward_total += 1
+        if pred[1] in ['0', '3']:
+            dir_correct += 1
+            forward_correct += 1
+
+print('\n文字方向准确率:', '%.2f' % (dir_correct/36))
+print('正向文字准确率:', '%.2f' % (forward_correct*100/forward_total))
+print('反向文字准确率:', '%.2f' % (reverse_correct*100/reverse_total))
+
+up, rt, dn, lt = [0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]
+up_total, rt_total, dn_total, lt_total = 0, 0, 0, 0
+for i, pred in enumerate(preds):
+
+    pred_name = pred[0].split('.jp')[0]
+    label = ''
+    for l in labels:
+        if l[0] == pred_name:
+            label = l
+            break
+
+    if label[1] == '0':
+        up_total += 1
+        up[int(pred[1])] += 1
+    elif label[1] == '1':
+        lt_total += 1
+        lt[int(pred[1])] += 1
+    elif label[1] == '2':
+        dn_total += 1
+        dn[int(pred[1])] += 1
+    elif label[1] == '3':
+        rt_total += 1
+        rt[int(pred[1])] += 1
+
+print('\n上向文档', '%.2f' % (up[0]*100/up_total), '%.2f' % (up[2]*100/up_total),
+      '%.2f' % (up[1]*100/up_total), '%.2f' % (up[3]*100/up_total))
+
+print('下向文档', '%.2f' % (dn[0]*100/dn_total), '%.2f' % (dn[2]*100/dn_total),
+      '%.2f' % (dn[1]*100/dn_total), '%.2f' % (dn[3]*100/dn_total))
+
+print('左向文档', '%.2f' % (lt[0]*100/lt_total), '%.2f' % (lt[2]*100/lt_total),
+      '%.2f' % (lt[1]*100/lt_total), '%.2f' % (lt[3]*100/lt_total))
+
+print('右向文档', '%.2f' % (rt[0]*100/rt_total), '%.2f' % (rt[2]*100/rt_total),
+      '%.2f' % (rt[1]*100/rt_total), '%.2f' % (rt[3]*100/rt_total))
